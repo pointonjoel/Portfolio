95440e6d48eb9eb17fa032aa5361d12c9daadd24
Joel Pointon <pointon.joel@Gmail.com>
2024-02-11 18:09:06 +0000

    add sharp

--- styles/projectStyles.js
+++ styles/projectStyles.js
@@ -1,15 +1,6 @@
 import styled from "styled-components";
 import css from "@styled-system/css";
 
-export const PDFContainer = styled.div(
-  css({
-    display: "flex",
-    margin: "0 auto",
-    width: ["90%", null, "70%"],
-    height: ["300px", "400px", "550px"],
-  })
-);
-
 export const Container = styled.div(
   css({
     display: "flex",
