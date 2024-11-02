bb98309b6451fdf67740d29e0f69891d579ef793
Joel Pointon <pointon.joel@Gmail.com>
2024-11-02 13:52:22 +0000

    use native form errors

--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -69,21 +69,10 @@ const Contact = () => {
   let nameError = "";
   let emailError = "";
   let messageError = "";
-  if (errors.name?.type === "required") {
-    nameError = "Please enter a name";
-  } else if (
-    errors.name?.type === "minLength" ||
-    errors.name?.type === "maxLength"
-  ) {
-    nameError = "Please enter a name of between 2 and 20 characters";
-  }
-  if (errors.email?.type === "required") {
-    emailError = "Please enter an email address";
-  } else if (errors.email?.type === "pattern") {
+  console.log("here!!", errors);
+  if (errors.reply_to?.type === "pattern") {
+    console.log("error here!");
     emailError = "Please enter a valid email address";
-  }
-  if (errors.message?.type === "required") {
-    messageError = "Please enter a message";
   }
   return (
     <>
@@ -99,8 +88,9 @@ const Contact = () => {
                 id="name"
                 type="text"
                 name="name"
+                required
                 {...register("name", {
-                  required: true,
+                  // required: true,
                   minLength: 2,
                   maxLength: 20,
                 })}
@@ -115,8 +105,8 @@ const Contact = () => {
                 id="reply_to"
                 type="text"
                 name="reply_to"
+                required
                 {...register("reply_to", {
-                  required: true,
                   pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
                 })}
               />
@@ -129,7 +119,8 @@ const Contact = () => {
               <TextArea
                 id="message"
                 name="message"
-                {...register("message", { required: true })}
+                required
+                {...register("message")}
               />
             </FormRow>
             <FormRow>
